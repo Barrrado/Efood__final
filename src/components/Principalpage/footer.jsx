ca4b252b3a201980dbfe2edeb89858aa36602c6a
Barrado <69765439+Barrrado@users.noreply.github.com>
2025-11-09 15:01:58 -0300

    Fix HTML entity in footer component

--- src/components/Principalpage/footer.jsx
+++ src/components/Principalpage/footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
       <FooterConteiner>
         <LogoFooter src={logofooter} alt="Logo" />
         <RedesConteiner>
-          <a href="https://github.com/Barrrado" target="_blank" rel="noreferrer">
+          <a href="https://github.com/Barrrado" target="_blank" rel="noreferrer'>
             <Redes src={git} alt="Git" />{' '}
           </a>
           <Redes src={face} alt="Face" />
