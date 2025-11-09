cdb888905bba5a2a759b8a5f18031f5ac96f6c01
Barrado <69765439+Barrrado@users.noreply.github.com>
2025-11-09 15:12:12 -0300

    Update base URL for checkout API

--- src/service/chekout.jsx
+++ src/service/chekout.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const checkoutApi = createApi({
   reducerPath: 'checkoutApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api-ebac.vercel.app/api/efood/restaurantes'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood/'
   }),
   endpoints: (builder) => ({
     checkout: builder.mutation({
